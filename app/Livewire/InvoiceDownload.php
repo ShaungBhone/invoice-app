b2e1362dabb9572c23d80b5778926adfb8a2a400
ShaungBhone <phonelaayy@gmail.com>
2024-03-15 01:00:08 +0630

    wip

--- app/Livewire/InvoiceDownload.php
+++ app/Livewire/InvoiceDownload.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Invoice;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Spatie\Browsershot\Browsershot;
 
+#[Layout('layouts.guest')]
 class InvoiceDownload extends Component
 {
     public function render()
     {
-        return view('livewire.invoice-download');
+        $html = view('livewire.invoice-download', [
+            'invoices' => Invoice::with(['items', 'customer'])->get(),
+        ])->render();
+
+        return 'Done';
     }
 }
